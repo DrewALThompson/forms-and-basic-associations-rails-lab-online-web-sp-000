fffc965acb8b3b8af3214474cb9aff6d84b32ff7
Drew Thompson <drewallynthompson@gmail.com>
2020-11-29 07:34:29 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,5 +4,9 @@ class Song < ActiveRecord::Base
   has_many :notes
   
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by(params[:name])
+    self.artist = Artist.find_or_create_by(name: name)
+  end 
+  
+  def artist_name
+    self.artist?
 end
